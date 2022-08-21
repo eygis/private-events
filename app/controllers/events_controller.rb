7f132dc19694e27e4dd08f65459dae18767c716b
eygis <ahill033@yahoo.com>
2022-08-21 19:01:01 +0900

    added event new and show views, create still not complete

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,41 @@
 class EventsController < ApplicationController
+
+  before_action :require_login, only: [:new, :create]
+
   def index
     @events = Event.all
     @users = User.all
   end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(event_params)
+
+    if @event.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  private
+
+  def require_login
+    unless user_signed_in?
+      flash[:error] = 'You must be logged in to do that.'
+      redirect_to root_path
+    end
+  end
+
+  def event_params
+    params.require(:event).permit(:event_date, :event_name, :creator)
+  end
+
 end
