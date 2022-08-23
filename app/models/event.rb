af8286c130dc86c0e4611403487994dfc3f8ff44
eygis <ahill033@yahoo.com>
2022-08-23 19:42:19 +0900

    added past and future scopes to event models

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,4 +4,7 @@ class Event < ApplicationRecord
     has_many :registrations
 
     has_many :attendees, through: :registrations, source: 'user'
+
+    scope :past, -> { where('event_date < ?', Date.today) }
+    scope :future, -> { where('event_date > ?', Date.today) }  
 end
